ea33cba33fcf2b04d43b6577eaa341c9323f503e
kongrith <capuchino_cartoon@hotmail.com>
2023-09-17 16:19:53 +0700

    init makefiles

---
+++ includes/ft.h
@@ -0,0 +1,7 @@
+#ifndef FT_H
+# define FT_H
+
+int				ft_isspace(char ch);
+
+
+#endif
---
+++ srcs/ft_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "ft.h"
+
+int	main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+   printf ("Hello from your first program!\n");
+   return 0;
+}
