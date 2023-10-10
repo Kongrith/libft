536620badc60365b0e6c0da727d9169f8bcb9cbd
Kongrith <capuchino_cartoon@hotmail.com>
2023-10-10 08:24:28 +0700

    correct conflict

---
+++ libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkomasat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 16:45:50 by kkomasat          #+#    #+#             */
+/*   Updated: 2023/10/06 15:30:39 by kkomasat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* */
+int	ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+/* */
+size_t ft_strlen(char *str);
+
+#endif
