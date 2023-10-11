8a1aef02e7db4183ab35a13f8b8c7a0df8b15984
Kongrith <capuchino_cartoon@hotmail.com>
2023-10-12 01:10:20 +0700

    add#3

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,47 +6,72 @@
 /*   By: kkomasat <kkomasat@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 14:25:03 by kkomasat          #+#    #+#             */
-/*   Updated: 2023/10/07 15:55:14 by kkomasat         ###   ########.fr       */
+/*   Updated: 2023/10/10 22:25:33 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* memcpy
 copy a memory block where source and destination do not overlap */
-
 #include <stdio.h>
 #include <string.h>
 
-void *ft_memcpy(void *restrict dest, const void * restrict src, size_t n)
+#define OVERLAP_FLAG 0
+
+void chk_overlap_mem(void *dest, void *src, size_t n)
 {
-	int	i;
-	char	*src_char = (char *)src;
-	char	*dest_char = (char *)dest;
-	
+	char *x;
+	char *y;
+	size_t	i;
+
+	x = dest;
+	y = src;
 	i = 0;
 	while (i < n)
 	{
-		dest_char[i] = src_char[i];
-		//*(dest_char + i) = *(src_char + i);
+    	if (x+i==y || y+i==x)
+		{
+			#undef  OVERLAP_FLAG
+			#define  OVERLAP_FLAG (x+i==y || y+i==x)
+		}
 		++i;
 	}
+	#if (OVERLAP_FLAG==1)
+		#warning The memory overlap between destination & source are detected
+	#endif
+}
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+
+	chk_overlap_mem(dest, (void *)src, n);
+	i = 0;
+	while (i < n)
+	{
+		*(char*)dest = *(char*)src;
+		++i;
+	}
+	return dest;
 }
 int main () {
-	char src[10] = "123456789";
-	//char dest1[10];
-	//char dest2[10];
-	//strcpy(dest1,"abcdefghijklmnopqrstwxyz");
-	//strcpy(dest2,"abcdefghijklmnopqrstwxyz");
-	//printf("Before memcpy dest = %s\n", dest1);
-	//memcpy(dest1, src, 4);
-	//printf("After memcpy dest = %s\n", dest1);
-	//ft_memcpy(dest2, src, 4);
-	//printf("After memcpy dest = %s\n", dest2);
-
-	printf("Before   memcpy dest = %s\n", src);
-	//memcpy(src+1, src, 8);
-	//printf("After    memcpy dest = %s\n", src);
-	ft_memcpy(src+1, src, 8);
-	printf("After ft_memcpy dest = %s\n", src);
+	char src1[] = "1234";
+	//char src2[] = "1234";
+	char dst1[] = "0000";
+	//char dst2[] = "0000";
 	
-	return(0);
-}
+	/* normal case */
+	printf("Before   memcpy dest = %s\n", dst1);
+	printf("After    memcpy dest = %s\n", (char*) memcpy(dst1, src1, 4));
+	//printf("After ft_memcpy dest = %s\n", (char*) ft_memcpy(dst2, src2, 4));
+	
+	/* stack smashing error */
+	//printf("Before   memcpy dest = %s\n", dst1);
+	//printf("After    memcpy dest = %s\n", (char*) memcpy(dst1, src1, 30));
+	//printf("After ft_memcpy dest = %s\n", (char*) ft_memcpy(dst2, src2, 30));  
+
+	/* problem case (overlap) */
+	//printf("Before   memcpy dest = %s\n", src1);
+	//printf("After    memcpy dest = %s\n", (char*) memcpy(src1+1, src1, 4));
+	//printf("After ft_memcpy dest = %s\n", (char*) ft_memcpy(src2+1, src2, 4));
+	
+	return(0);}
