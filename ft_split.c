45ced03bce914161c9b8cf78b4507175698679c1
orange <orange@Macintosh.local>
2023-10-24 10:45:40 +0700

    add ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,180 @@
 /*   By: kkomasat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:44:45 by kkomasat          #+#    #+#             */
-/*   Updated: 2023/10/18 22:45:19 by kkomasat         ###   ########.fr       */
+/*   Updated: 2023/10/24 02:32:01 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*	ft_split
+*	string:		"Hello,World,Split,Me"
+*	delimeter:	','
+*	return:		["Hello", "World", "Split", "Me", NULL]
+*/
+
 #include "libft.h"
+#include <stdio.h>
+
+size_t	count_char_section(char *ptr, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	i = 0;
+	while(*(ptr + i) != '\0')
+	{
+		//printf("%c", *(ptr + i));
+		if ( *(ptr + i) != c)
+		{
+			while( (*(ptr + i) != c) && (*(ptr + i) != '\0'))
+			{
+				++i;
+			}
+			++count;
+		}
+		else
+		{
+			++i;
+		}
+	}
+	return (count);
+}
+
+char	*find_string(char *ptr, char c)
+{
+	size_t	i;
+	size_t  char_flag;
+	
+	if (ptr[0] == c)
+		char_flag = 0;
+	else
+		char_flag = 1;
+	printf("find string - char flag:%ld\n", char_flag);
+	i = 0;
+	//printf("find string\n");
+	while (*(ptr + i) != '\0')
+	{
+		//printf("%ld\n", i);
+		if (char_flag == 1)
+		{
+			//while ( (*(ptr + i) != c) && (*(ptr + i) != '\0'))
+			//{
+			//	++i;
+			//}
+			//printf("i: %ld\n", i);
+			return (ptr + i);
+		}
+		else
+		{
+			while ( (*(ptr + i) == c) && (*(ptr + i) != '\0'))
+			{
+				++i;
+			}
+			return (ptr + i);
+		}	
+	}
+	return (0);
+}
+
+char	*split_string(char *s, char c)
+{
+	char	*string;
+	char	*ptr;
+	size_t	len;
+	size_t	char_flag;
+	size_t	i;
+
+	ptr = s;
+	if (ptr[0] == c)
+		char_flag = 0;
+	else
+		char_flag = 1;
+	//printf("split string - char flag:%ld\n", char_flag);
+	len = 0;
+	i = 0;
+	while (*(ptr + i) != '\0')
+	{
+		//printf("%s\n", ptr + i);
+		if (char_flag == 0)
+		{
+			//printf("%ld\n", i);
+			if (*(ptr + i) == c)
+			{
+				//printf("case1 %ld\n", i);
+				++i;
+				++s;
+			}
+			else
+			{
+				//printf("case2 %ld\n", i);
+				while ( (*(ptr + i) != c) && (*(ptr + i) != '\0')) 
+				{
+					++i;
+					++len;
+				}
+				printf("len: %ld\n", len);
+				string = (char *)malloc(sizeof(char) * (len + 1));
+				ft_strlcpy(string, s, len+1);
+				printf("%s\n", string);
+				return(string);
+			}
+		}
+		else
+		{
+			//printf("%ld\n", i);
+			while ( (*(ptr + i) != c) && (*(ptr + i) != '\0'))
+			{
+				++i;
+				++len;
+			}
+			string = (char *)malloc(sizeof(char) * (len + 1));
+			ft_strlcpy(string, s, len+1);
+			return(string);
+		}
+	}
+	string = (char *)malloc(sizeof(char) * (len + 1));
+	ft_strlcpy(string, s, len+1);
+	return(string);
+}
 
 char    **ft_split(char const *s, char c)
 {
+	char	**string_array;
+	char	*ptr;
+	size_t	num_split;
+	size_t	i;
+	
+	if (s == NULL)
+		return (NULL);
+	else
+		ptr = (char *) s;
+	num_split = count_char_section(ptr, c);
+	//printf("len:%ld\n", num_split);
+	string_array = (char **) malloc (sizeof(char *) * (num_split + 1));
+	if (string_array == NULL)
+		return (NULL);
+	else
+		string_array[num_split + 1] = NULL;
+	i = 0;
+	ptr = find_string(ptr, c);
+	while (i < num_split)
+	{
+		//printf("string array: %ld\n", i);
+		string_array[i] = split_string(ptr, c);
+		ptr = find_string(ptr, c);
+		++i;
+	}
+	return (string_array);
 }
+/*
+int	main()
+{
+	char	*s1 = "Hello,World,Split,Me";
+	char	delimiter = ',';
+	char	**pointer_arrays;
+	pointer_arrays = ft_split(s1, delimiter);
+	printf("original: %s\n", s1);
+	for(int i = 0; i<4; i++)
+      printf("%s\n", pointer_arrays[i]);
+	//printf("ft_split: %s\n", ft_split(s1, delimiter)[0]);
+}*/
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,150 +6,43 @@
 /*   By: kkomasat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:42:39 by kkomasat          #+#    #+#             */
-/*   Updated: 2023/10/21 17:18:01 by kkomasat         ###   ########.fr       */
+/*   Updated: 2023/10/22 00:39:06 by kkomasat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-char	*custom_strstr(char *str, char *substr)
-{
-	static char	*ptr;
 
-	//if ((ft_strlen(str) == 0) && (ft_strlen(substr) == 0))
-	//	return("");	
-	ptr = str;
-	while (*ptr)
-	{
-		if (ft_strncmp(ptr, substr, ft_strlen(substr)) == 0)
-			return (ptr);
-		++ptr;
-	}
-	return (0);
+static int		is_in_set(char c, const char *set)
+{
+	while (*set)
+		if (c == *set++)
+			return (0);
+	return (1);
 }
 
-void custom_strcat(char *dest, size_t len)
+char			*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-
-	i = 0;
-	while (dest[i + len] != '\0')
-	{
-		printf("%c\n", dest[i + len]);
-		dest[i] = dest[i + len];
-		++i;
-	}
-	dest[i] = '\0';
-}
-
-void ft_removesubstr(char *str, char *substr)
-{
-	char	*ptr;
-	size_t	i;
-
-	i = 0;
-	//ptr = ft_strstr( &str[i], substr);
-	while ( i < (ft_strlen(str) - ft_strlen(substr) + 1))
-	{
-		ptr = custom_strstr( &str[i], substr);
-		if (ptr)
-		{
-			printf("ptr:%c\n", ptr[i]);
-			custom_strcat(ptr, 2);
-			i = i + ft_strlen(substr); 
-		}
-		else
-		{
-			++i;
-		}
-	}
-}
-
-
-char *ft_strtrim(char const *s1, char const *set)
-{
-	char	*ptr;
+	size_t	start;
+	size_t	end;
+	char	*rtn;
 
 	if (!s1)
 		return (NULL);
-	if (ft_strlen((char *) s1) == 0)
-		return(0);
-	if (!set || ft_strlen((char *) set) == 0)
+	if (!set)
 		return (ft_strdup(s1));
-
-	ptr = (char *) malloc(sizeof(char) * (ft_strlen( (char *) s1) + 1));
-	if (!ptr)
-		return (NULL);
-	ptr = (char *) s1;
-	ft_removesubstr(ptr, (char *) set);
-	//printf("s1 :%p\n", s1);
-	//printf("ptr:%p\n", ptr);
-	return ((char *) s1);
-}
-
-/*
-int main()
-{
-    char str[] = "abcdefgh";
-	char substr[] = "de";
-	
-	printf("%s\n", ft_strtrim(str, substr));
-
-    ptr = ft_strstr(str, substr);
-    if (ptr != NULL)
-        printf("%s\n", ptr);
-    else
-        printf("not found\n");
-*/
-
-/*
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*ptr;
-	size_t	i;
-	size_t	j;
-	size_t	max;
-	
-	if (!s1)
-		return (NULL);
-	if (ft_strlen((char *) s1) == 0)
-		return(0);
-	if (!set || ft_strlen((char *) set) == 0)
-		return (ft_strdup(s1));
-
-	max = ft_strlen((char *) s1) - ft_strlen((char *) set) + 1;
-	ptr = (char *) malloc(sizeof(char) * (ft_strlen((char *) s1) + 1));
-	if (ptr == NULL)
-		exit(0);
-		
-	//if (ft_strlen((char *) set) == 0)
-	//	return (ft_strdup(s1));
-	//printf("original ptr: %s\n", ptr);	
-	i = 0; 
-	while (i < max)
+	start = 0;
+	end = ft_strlen( (char *) s1);
+	while (is_in_set(s1[start], set) == 0)
+		start++;
+	if (start == ft_strlen( (char *) s1))
 	{
-		j = 0;
-		//printf("i: %ld ", i);
-		if (s1[i + j] != set[j])
-			ptr[i] = s1[i];
+		if (!(rtn = ft_strdup("")))
+			return (NULL);
 		else
-		{
-			while (s1[i + j] == set[j])
-			{
-				//printf("j:%ld %c ", j, set[j]);
-				++j;
-				if (set[j] == '\0')
-				{
-					ft_memmove( &ptr[i], &s1[i + ft_strlen((char *) set)], ft_strlen((char *) s1) - i  - ft_strlen((char *) set));
-					i += ft_strlen((char *) set);
-				}	
-			}
-		}
-		//printf("\n");
-		//printf("%s\n", ptr);
-		if (set[j] != '\0')
-			++i;
+			return (rtn);
 	}
-	ptr[max] = '\0';
-	return (ptr);
-}*/
+	while (is_in_set(s1[end - 1], set) == 0)
+		end--;
+	rtn = ft_substr(s1, start, end - start);
+	return (rtn);
+}
